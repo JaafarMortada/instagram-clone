4c6247247fb5e0152618e91e7e1767a417dee824
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-10 00:46:57 +0300

    Backend <> Added Like API

--- backend/app/Http/Controllers/LikeController.php
+++ backend/app/Http/Controllers/LikeController.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Like;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
-    //
+    public function like($post_id){
+        $like = new Like;
+        $like->post_id = intval($post_id);
+        $like->user_id = Auth::id();
+        $like->save();
+        return response()->json(["status" => "liked successfully"]);
+    }
 }
