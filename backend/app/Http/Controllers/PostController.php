13cd2037416bd19578ba63e2422970eae0a77605
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-10 00:17:11 +0300

    Backend <> Added addPost API

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 class PostController extends Controller
 {
-    //
+    function addPost(Request $request){
+        $post = new Post;
+        $post->user_id = Auth::id();
+        $post->description = $request->description;
+        $file_name = time()."post_image".".".$request->image_path->extension();
+        $request -> image_path ->move(storage_path('images'),$file_name);
+        $post-> image_path = storage_path("images"). "\\" .$file_name;
+        $post->save();
+
+        return response()->json(["status" => $post]);
+    }
+
+
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
-
+use App\Http\Controllers\PostController;
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
 });
+
+Route::post('/add_post', [PostController::class, "addPost"]);
